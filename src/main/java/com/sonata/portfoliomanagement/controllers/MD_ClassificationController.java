15daf34a427ccbebc5e4bc883c48161f3d8c319c
Naini Rakesh <naini.rakesh@sonata-software.com>
2024-06-17 11:04:26 +0530

    updated admin services

--- src/main/java/com/sonata/portfoliomanagement/controllers/MD_ClassificationController.java
+++ src/main/java/com/sonata/portfoliomanagement/controllers/MD_ClassificationController.java
@@ -1,29 +1,28 @@
 package com.sonata.portfoliomanagement.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-
-import com.sonata.portfoliomanagement.interfaces.MD_ClassificationRepository;
-import com.sonata.portfoliomanagement.model.MD_Classification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
+import com.sonata.portfoliomanagement.interfaces.MD_ClassificationRepository;
+import com.sonata.portfoliomanagement.model.MD_Classification;
 @CrossOrigin(origins = "http://localhost:5173" )
 @RestController
-@RequestMapping("/classification")
+@RequestMapping("/classifications")
 public class MD_ClassificationController {
 
     @Autowired
-    private MD_ClassificationRepository classificationRepository;
+    private MD_ClassificationRepository classificationRepo;
 
     @GetMapping("/get")
     public ResponseEntity<List<MD_Classification>> getAllClassifications() {
-        List<MD_Classification> classifications = classificationRepository.findAll();
+        List<MD_Classification> classifications = classificationRepo.findAll();
         return ResponseEntity.ok(classifications);
     }
 
@@ -31,34 +30,64 @@ public class MD_ClassificationController {
     public ResponseEntity<List<MD_Classification>> createClassifications(@RequestBody List<MD_Classification> classificationList) {
         List<MD_Classification> createdClassifications = new ArrayList<>();
         for (MD_Classification classification : classificationList) {
-            MD_Classification createdClassification = classificationRepository.save(classification);
+            MD_Classification createdClassification = classificationRepo.save(classification);
             createdClassifications.add(createdClassification);
         }
         return new ResponseEntity<>(createdClassifications, HttpStatus.CREATED);
     }
+    @PutMapping("/update")
+    public ResponseEntity<String> updateClassifications(@RequestBody List<MD_Classification> classifications) {
+        List<Integer> notFoundIds = classifications.stream()
+                .filter(classificationDetails -> {
+                    Integer id = classificationDetails.getId();
+                    if (id == null) {
+                        return true;
+                    }
+                    Optional<MD_Classification> existingClassification = classificationRepo.findById(id);
+                    if (existingClassification.isPresent()) {
+                        MD_Classification classificationToUpdate = existingClassification.get();
+                        classificationToUpdate.setClassification(classificationDetails.getClassification());
+                        classificationRepo.save(classificationToUpdate);
+                        return false;
+                    } else {
+                        return true;
+                    }
+                })
+                .map(MD_Classification::getId)
+                .collect(Collectors.toList());
 
-    @PutMapping("/update/{ids}")
-    public ResponseEntity<List<MD_Classification>> updateClassifications(@PathVariable List<Integer> ids, @RequestBody List<MD_Classification> updatedClassifications) {
-        List<MD_Classification> updatedEntities = new ArrayList<>();
-        for (int i = 0; i < ids.size(); i++) {
-            int id = ids.get(i);
-            MD_Classification updatedClassification = updatedClassifications.get(i);
-            Optional<MD_Classification> existingClassificationOptional = classificationRepository.findById(id);
-            if (existingClassificationOptional.isPresent()) {
-                updatedClassification.setId(id);
-                updatedEntities.add(classificationRepository.save(updatedClassification));
-            }
+        if (!notFoundIds.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("No classifications found with IDs: " + notFoundIds.toString());
         }
-        return ResponseEntity.ok(updatedEntities);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("Classifications have been successfully updated.");
     }
 
-    @DeleteMapping("/delete/{ids}")
-    public ResponseEntity<Void> deleteClassifications(@PathVariable List<Integer> ids) {
-        for (Integer id : ids) {
-            if (classificationRepository.existsById(id)) {
-                classificationRepository.deleteById(id);
-            }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> deleteClassificationsByIds(@RequestBody List<Integer> ids) {
+        List<Integer> notFoundIds = ids.stream()
+                .filter(id -> {
+                    Optional<MD_Classification> classification = classificationRepo.findById(id);
+                    if (classification.isEmpty()) {
+                        return true;
+                    }
+                    classificationRepo.deleteById(id);
+                    return false;
+                })
+                .collect(Collectors.toList());
+
+        if (!notFoundIds.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("No classifications found with IDs: " + notFoundIds.toString());
         }
-        return ResponseEntity.noContent().build();
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("Classifications with specified IDs have been deleted.");
     }
+
+
+
 }
--- src/main/java/com/sonata/portfoliomanagement/controllers/MD_DeliveryDirectorController.java
+++ src/main/java/com/sonata/portfoliomanagement/controllers/MD_DeliveryDirectorController.java
@@ -3,16 +3,18 @@ package com.sonata.portfoliomanagement.controllers;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-import com.sonata.portfoliomanagement.interfaces.MD_DeliveryDirectorRepository;
-import com.sonata.portfoliomanagement.model.MD_DeliveryDirector;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import com.sonata.portfoliomanagement.interfaces.MD_DeliveryDirectorRepository;
+import com.sonata.portfoliomanagement.model.MD_DeliveryDirector;
 @CrossOrigin(origins = "http://localhost:5173" )
 @RestController
-@RequestMapping("/mddd")
+@RequestMapping("/dd")
 public class MD_DeliveryDirectorController {
 
     @Autowired
@@ -20,8 +22,8 @@ public class MD_DeliveryDirectorController {
 
     @GetMapping("/get")
     public ResponseEntity<List<MD_DeliveryDirector>> getAllDeliveryDirectors() {
-        List<MD_DeliveryDirector> deliveryDirectors = deliveryDirectorRepository.findAll();
-        return ResponseEntity.ok(deliveryDirectors);
+        List<MD_DeliveryDirector> directorsList = deliveryDirectorRepository.findAll();
+        return ResponseEntity.ok(directorsList);
     }
 
     @PostMapping("/save")
@@ -34,28 +36,56 @@ public class MD_DeliveryDirectorController {
         return new ResponseEntity<>(createdDeliveryDirectors, HttpStatus.CREATED);
     }
 
-    @PutMapping("/update/{ids}")
-    public ResponseEntity<List<MD_DeliveryDirector>> updateDeliveryDirectors(@PathVariable List<Integer> ids, @RequestBody List<MD_DeliveryDirector> updatedDeliveryDirectors) {
-        List<MD_DeliveryDirector> updatedEntities = new ArrayList<>();
-        for (int i = 0; i < ids.size(); i++) {
-            int id = ids.get(i);
-            MD_DeliveryDirector updatedDeliveryDirector = updatedDeliveryDirectors.get(i);
-            Optional<MD_DeliveryDirector> existingDeliveryDirectorOptional = deliveryDirectorRepository.findById(id);
-            if (existingDeliveryDirectorOptional.isPresent()) {
-                updatedDeliveryDirector.setId(id);
-                updatedEntities.add(deliveryDirectorRepository.save(updatedDeliveryDirector));
-            }
+
+    @PutMapping("/update")
+    public ResponseEntity<String> updateDeliveryDirectors(@RequestBody List<MD_DeliveryDirector> directors) {
+        List<Integer> notFoundIds = directors.stream()
+                .filter(directorDetails -> {
+                    Integer id = directorDetails.getId();
+                    if (id == null) {
+                        return true;
+                    }
+                    Optional<MD_DeliveryDirector> existingDirector = deliveryDirectorRepository.findById(id);
+                    if (existingDirector.isPresent()) {
+                        MD_DeliveryDirector directorToUpdate = existingDirector.get();
+                        directorToUpdate.setDeliveryDirector(directorDetails.getDeliveryDirector());
+                        deliveryDirectorRepository.save(directorToUpdate);
+                        return false;
+                    } else {
+                        return true;
+                    }
+                })
+                .map(MD_DeliveryDirector::getId)
+                .collect(Collectors.toList());
+
+        if (!notFoundIds.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("No delivery directors found with IDs: " + notFoundIds.toString());
         }
-        return ResponseEntity.ok(updatedEntities);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("Delivery directors have been successfully updated.");
     }
 
-    @DeleteMapping("/delete/{ids}")
-    public ResponseEntity<Void> deleteDeliveryDirectors(@PathVariable List<Integer> ids) {
-        for (Integer id : ids) {
-            if (deliveryDirectorRepository.existsById(id)) {
-                deliveryDirectorRepository.deleteById(id);
-            }
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> deleteDeliveryDirectorsByIds(@RequestBody List<Integer> ids) {
+        List<Integer> notFoundIds = ids.stream()
+                .filter(id -> {
+                    Optional<MD_DeliveryDirector> director = deliveryDirectorRepository.findById(id);
+                    if (director.isEmpty()) {
+                        return true;
+                    }
+                    deliveryDirectorRepository.deleteById(id);
+                    return false;
+                })
+                .collect(Collectors.toList());
+
+        if (!notFoundIds.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("No delivery directors found with IDs: " + notFoundIds.toString());
         }
-        return ResponseEntity.noContent().build();
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("Delivery directors with specified IDs have been deleted.");
     }
 }
