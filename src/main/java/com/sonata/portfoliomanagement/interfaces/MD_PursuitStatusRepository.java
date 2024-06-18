f018139274a0736405fca33287de5193a6840bc5
Naini Rakesh <naini.rakesh@sonata-software.com>
2024-06-18 15:36:15 +0530

    updated roles and pursuit probability services

--- src/main/java/com/sonata/portfoliomanagement/interfaces/MD_PursuitStatusRepository.java
+++ src/main/java/com/sonata/portfoliomanagement/interfaces/MD_PursuitStatusRepository.java
@@ -3,9 +3,5 @@ package com.sonata.portfoliomanagement.interfaces;
 import com.sonata.portfoliomanagement.model.MD_PursuitStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface MD_PursuitStatusRepository extends JpaRepository<MD_PursuitStatus, Integer> {
-    Optional<MD_PursuitStatus> findByPursuitStatus(String pursuitStatus);
 }
-
